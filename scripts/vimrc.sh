52ef1e880125945c893d384f4c863b62a7add534
Randy Stauner <randy@magnificent-tears.com>
2012-11-06 14:03:34 -0700

    Fix vimrc perms and symlinks

--- scripts/vimrc.sh
+++ scripts/vimrc.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 name=vimrc
+umask 0022
 
 if ! [[ -d "$name" ]]; then
   remote="`git config remote.origin.url`"
   git clone "${remote/run_control/$name}" "$name"
 fi
 
-[[ -e "$HOME/.vim"   ]] || ln -s "run_control/$name" "$HOME/.vim"
-[[ -e "$HOME/.vimrc" ]] || ln -s ".vim/vimrc"        "$HOME/.vimrc"
+[[ -e "$HOME/.vim"   ]] || ln -sf "run_control/$name" "$HOME/.vim"
+[[ -e "$HOME/.vimrc" ]] || ln -sf ".vim/vimrc"        "$HOME/.vimrc"
