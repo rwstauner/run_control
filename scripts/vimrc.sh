13d9647271bf73cbe2a48b929e7700bd487da7be
Randy Stauner <randy@magnificent-tears.com>
2012-12-11 21:53:41 -0700

    Run 'make' in 'vimrc' after cloning

--- scripts/vimrc.sh
+++ scripts/vimrc.sh
@@ -6,6 +6,7 @@ umask 0022
 if ! [[ -d "$name" ]]; then
   remote="`git config remote.origin.url`"
   git clone "${remote/run_control/$name}" "$name"
+  make -C "$name"
 fi
 
 [[ -e "$HOME/.vim"   ]] || ln -sf "run_control/$name" "$HOME/.vim"
