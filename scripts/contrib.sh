5a88ea3de27c98003791e2d66b433c99cdde234d
Randy Stauner <randy@magnificent-tears.com>
2013-05-29 14:46:57 -0700

    Download jq for cli json processing

--- scripts/contrib.sh
+++ scripts/contrib.sh
@@ -4,9 +4,15 @@ bin="$HOME/bin/contrib"
 completion="$HOME/.bashrc.d/completion.d/contrib"
 mkdir -p "$bin" "$completion"
 
+if uname -m -i -p | sed 's/ /\n/g' | uniq | grep -q x86_64; then
+  bit=64
+else
+  bit=32
+fi
+
 function dl () {
   dest="$1" url="$2"
-  test -e "$dest" || curl -sL "$url" > "$dest"
+  test -s "$dest" || wget "$url" -O "$dest"
 }
 
 function dl_bin () {
@@ -22,5 +28,6 @@ function dl_completion () {
 dl_completion django https://github.com/django/django/raw/master/extras/django_bash_completion
 
 dl_bin  es      http://download.elasticsearch.org/es2unix/es
+dl_bin  jq      http://stedolan.github.io/jq/download/linux${bit}/jq
 dl_bin  hub     http://defunkt.io/hub/standalone
 dl_bin  viack   https://github.com/tsibley/viack/raw/master/viack
