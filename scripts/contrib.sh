7ffb9d3bda1b937a2200a1cc8ea9880e40071976
Randy Stauner <randy@magnificent-tears.com>
2013-05-29 14:38:34 -0700

    Download standalone bin/ scripts

--- scripts/contrib.sh
+++ scripts/contrib.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+bin="$HOME/bin/contrib"
 completion="$HOME/.bashrc.d/completion.d/contrib"
-mkdir -p "$completion"
+mkdir -p "$bin" "$completion"
 
 function dl () {
   dest="$1" url="$2"
   test -e "$dest" || curl -sL "$url" > "$dest"
+}
+
+function dl_bin () {
+  dest="$bin/$1"
+  dl "$dest" "$2"
+  chmod 0755 "$dest"
 }
 
 function dl_completion () {
@@ -13,3 +20,7 @@ function dl_completion () {
 }
 
 dl_completion django https://github.com/django/django/raw/master/extras/django_bash_completion
+
+dl_bin  es      http://download.elasticsearch.org/es2unix/es
+dl_bin  hub     http://defunkt.io/hub/standalone
+dl_bin  viack   https://github.com/tsibley/viack/raw/master/viack
