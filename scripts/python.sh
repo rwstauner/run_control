d33abe5ea9a25fee49cdb6f0de19aec2c63c8a76
Randy Stauner <randy@magnificent-tears.com>
2013-06-17 20:33:25 -0700

    Create local venv and pip install into it
    instead of leaving hints in the notes file

--- scripts/python.sh
+++ scripts/python.sh
@@ -7,3 +7,25 @@ pyrc=$pydir/rc
 if ! [[ -d $pyrc ]]; then
   ln -s ~/run_control/python $pyrc;
 fi
+
+pyver=`python -V 2>&1 | cut -d ' ' -f 2`
+
+if echo "$pyver" | grep -qE '[0-9]+\.[0-9]+\.[0-9]+'; then
+  echo "Python: $pyver"
+else
+  echo 'Failed to get python version' 1>&2
+  exit 1
+fi
+
+venv=$pydir/venv/$pyver
+venv_local=$pydir/venv/local
+activate=$venv/bin/activate
+
+if ! [[ -e "$activate" ]]; then
+  virtualenv $venv --distribute
+fi
+
+test -h $venv_local || ln -s $pyver $venv_local
+
+test -f $activate && . $activate && \
+  pip install -r $pyrc/requirements.txt
