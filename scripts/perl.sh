4c5e549e85605b775de6e96810d451558f42f5ed
Randy Stauner <randy@magnificent-tears.com>
2013-06-18 20:16:29 -0700

    Setup ~/perl5/rc and install perlbrew and cpanm

---
+++ scripts/perl.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+perldir=$HOME/perl5
+test -d "$perldir" || mkdir "$perldir"
+
+perlrc=$perldir/rc
+if ! [[ -d $perlrc ]]; then
+  ln -s ~/run_control/perl $perlrc;
+fi
+
+pbrc=$perldir/perlbrew/etc/bashrc
+# get perlbrew if it's already installed
+test -f "$pbrc" && . "$pbrc"
+
+which perlbrew &> /dev/null || \
+  curl -L install.perlbrew.pl | bash
+
+# update/activate perlbrew
+test -f "$pbrc" && . "$pbrc"
+
+which cpanm    &> /dev/null || \
+  perlbrew install-cpanm
+
+# TODO: perlbrew@std or local::lib
