9a2f18c7976a9d90399a3b195f3ff6d870e62bc8
Randy Stauner <randy@magnificent-tears.com>
2013-07-15 20:14:28 -0700

    Symlink vagrant exe into bin instead of using PATH
    Since there's only one executable put one less dir in $PATH.

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+bin=$HOME/bin/contrib
+
 function ensure_line () {
   line="$1" file="$2"
   grep -qFx "$line" "$file" || echo "$line" >> "$file"
 }
 
 ensure_line "$(cat ~/run_control/vagrant/loader.rb)" ~/.vagrant.d/Vagrantfile
+
+# TODO: /opt/vagrant/bin/* ?
+# At that point it's probably better to just put it back in $PATH.
+
+for script in vagrant; do
+  ln -sf /opt/vagrant/bin/$script $bin/$script
+done
+
+# TODO: test -e $bin/packer || unzip
