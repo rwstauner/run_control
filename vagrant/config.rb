0a81d4352765a5986d4c9563a957eb610cb9a2b2
Randy Stauner <randy@magnificent-tears.com>
2013-08-31 11:17:08 -0700

    Always forward at least one port to the vagrant vm
    so that we can use one without needing to reload.

--- vagrant/config.rb
+++ vagrant/config.rb
@@ -4,14 +4,26 @@ Vagrant.configure('2') do |config|
   # Overwrite default ssh port.
   config.vm.network :forwarded_port, {
     :guest        => 22,
-    :host         => 3199,
+    :host         => 3200,
     :host_ip      => '127.0.0.1',
     :id           => 'ssh',
     :auto_correct => true,
   }
 
   # Confirm that using alternate port range works.
-  config.vm.usable_port_range = (3200 .. 3250)
+  config.vm.usable_port_range = (3200 .. 3300)
+
+  # Always forward at least one port (so you can use one without restarting)..
+  # NOTE: This can cause a suspended vm to not resume when a port conflict
+  # is detected.  Resuming the vm in the vbox gui and shutting down fixes it.
+  # I think I've only had to do this once for any given suspended vm, but if
+  # it happens more often we could try saving the list of used ports somewhere.
+  config.vm.network :forwarded_port, {
+    :guest        => 3201,
+    :host         => 3201,
+    :id           => 'vagrant_rc',
+    :auto_correct => true,
+  }
 
   # Bridge a network connection without prompting for which NIC:
   #config.vm.network :public_network, :bridge => 'wlan0'
