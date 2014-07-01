904ac895539820e48bb5c16c9e8185a96989cc71
Randy Stauner <randy@magnificent-tears.com>
2014-07-01 08:54:03 -0700

    Don't let ohmyzsh cripple my LESS options

--- zsh/loader.zsh
+++ zsh/loader.zsh
@@ -5,4 +5,7 @@ source $rc_dir/sh/loader.sh
 zshrc_dir=$HOME/run_control/zsh
 source_rc_files $zshrc_dir/zsh.d/* ~/.zshrc.local $EXTRA_ZSHRC
 
+# OMZ criples my LESS options so restore them.
+source $rc_dir/sh/sh.d/less.sh
+
 unset rc_dir zshrc_dir
