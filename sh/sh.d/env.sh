3bd311fd87a1c5268d70c839b6e3491f8ae09dc2
Randy Stauner <randy@magnificent-tears.com>
2014-07-01 08:48:34 -0700

    Move misc env vars from bash_profile to sh.d/env

--- sh/sh.d/env.sh
+++ sh/sh.d/env.sh
@@ -1,4 +1,14 @@
+# Environment variables that don't have a better place.
+
+export BROWSER=firefox
+export FTP_PASSIVE=1; # used by Net::FTP, and maybe possibly hopefully some other things
+
+#export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig/:/usr/lib/pkgconfig/
+
+# Universal file exclusions
 export TAR_OPTIONS="$TAR_OPTIONS --exclude-from=$HOME/.excludes"
+
+export XML_CATALOG_FILES="$HOME/devel/xml/catalog /etc/xml/catalog"
 
 if [[ -z "$LS_COLORS" ]]; then
   if which dircolors &> /dev/null; then
