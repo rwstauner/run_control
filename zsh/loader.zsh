735fad455825107c2e8ba19d7bd1779184b3e368
Randy Stauner <randy@magnificent-tears.com>
2014-07-02 08:22:48 -0700

    Load ohymzsh earlier so I can overwrite
    NO I DON'T WANT TO SHARE HISTORY INCREMENTALLY.

--- zsh/loader.zsh
+++ zsh/loader.zsh
@@ -3,9 +3,14 @@ rc_dir=$HOME/run_control
 source $rc_dir/sh/loader.sh
 
 zshrc_dir=$HOME/run_control/zsh
-source_rc_files $zshrc_dir/zsh.d/* ~/.zshrc.local $EXTRA_ZSHRC
+
+# Load OMZ first so that we can overwrite.
+source_rc_files $zshrc_dir/ohmy.zsh
 
 # OMZ criples my LESS options so restore them.
 source $rc_dir/sh/sh.d/less.sh
+
+# Load everything else.
+source_rc_files $zshrc_dir/zsh.d/* ~/.zshrc.local $EXTRA_ZSHRC
 
 unset rc_dir zshrc_dir
