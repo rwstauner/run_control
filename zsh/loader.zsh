551f52efeecde5d804d8f0da0a46a612a18b744e
Randy Stauner <randy@magnificent-tears.com>
2014-07-03 08:08:10 -0700

    Load OMZ before *everything* so we can overwrite *anything*
    ugh.

--- zsh/loader.zsh
+++ zsh/loader.zsh
@@ -1,14 +1,11 @@
 rc_dir=$HOME/run_control
 
-source $rc_dir/sh/loader.sh
-
-zshrc_dir=$HOME/run_control/zsh
+zshrc_dir=$rc_dir/zsh
 
 # Load OMZ first so that we can overwrite.
-source_rc_files $zshrc_dir/ohmy.zsh
+source $zshrc_dir/ohmy.zsh
 
-# OMZ criples my LESS options so restore them.
-source $rc_dir/sh/sh.d/less.sh
+source $rc_dir/sh/loader.sh
 
 # Load everything else.
 source_rc_files $zshrc_dir/zsh.d/* ~/.zshrc.local $EXTRA_ZSHRC
