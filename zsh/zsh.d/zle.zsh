519449601eb0c26653bf60412b284b0c12333928
Randy Stauner <randy@magnificent-tears.com>
2014-07-08 07:07:15 -0700

    Use match-words-by-style for shell word movements

--- zsh/zsh.d/zle.zsh
+++ zsh/zsh.d/zle.zsh
@@ -47,10 +47,13 @@ bindkey "\e+" predict-toggle
 # Comment line instead of executing or eliminating.
 bindkey "\e#" pound-insert
 
+
 # Up/Down searches history matching the text before the cursor.
 # https://coderwall.com/p/oqtj8w
+# See https://github.com/zsh-users/zsh-history-substring-search for an alternate implementation.
 bindkey "\e[A" history-beginning-search-backward
 bindkey "\e[B" history-beginning-search-forward
+
 # The following will do the same but put the cursor at the end of the line.
 # That's consistent with bash/readline when you press up with no prefix
 # but that sometimes gets me into trouble; maybe the extra key press isn't bad.
@@ -65,18 +68,13 @@ bindkey "\e[B" history-beginning-search-forward
 bindkey "^U" backward-kill-line
 
 
-# Ctrl-arrow  moves across words (small words).
-# Shift-arrow moves across non-blank words.
-bindkey "\e[1;2D" vi-backward-blank-word
-bindkey "\e[1;2C" vi-forward-blank-word
-
-
 # Help
 bindkey "\e^V" describe-key-briefly
 bindkey "\e\`" which-command
 
 # Like vim's :
 bindkey "\e:"  execute-named-cmd
+
 
 # Keys to remember:
 
@@ -91,19 +89,34 @@ bindkey "\e:"  execute-named-cmd
 # TODO: Create function (omz plugin?) to display cheat sheet of zle commands.
 # TODO: is there a quote-this-word function?
 
-# Kill non-blank regions backward and forward.
-backward-kill-blank-word () {
-  set-mark-command
-  # This function seems to be the slow one. :-/
-  vi-backward-blank-word
-  kill-region
+
+# To change word-style for all functions:
+#autoload -Uz select-word-style
+#select-word-style normal
+
+# Move/Kill shell words (better than non-blank regions) backward and forward.
+() {
+  local direction action prefix match_fun
+  for direction in backward forward; do
+    for action in '' 'kill'; do
+      prefix="${direction}${action:+-}${action}"
+      # Set word-style for our widget.
+      zstyle ":zle:${prefix}-shell-word" word-style shell
+      # (There is no "forward-kill-*" it's just "kill-*".)
+      match_fun="${prefix//forward-kill/kill}-word-match"
+      # (Functions autoloaded by select-word-style aren't named *-match.)
+      autoload -Uz "$match_fun"
+      # Alias our widgets to the magic *-match functions (which use the style).
+      zle -N "${prefix}-shell-word" "$match_fun"
+    done
+  done
 }
-forward-kill-blank-word () {
-  set-mark-command
-  vi-forward-blank-word
-  kill-region
-}
-zle -N backward-kill-blank-word
-zle -N forward-kill-blank-word
-bindkey "\e[" backward-kill-blank-word
-bindkey "\e]" forward-kill-blank-word
+
+bindkey "\e[" backward-kill-shell-word
+bindkey "\e]"  forward-kill-shell-word
+
+# Ctrl-arrow  moves across words ($WORDCHARS).
+# Shift-arrow moves across shell words.
+# TODO: use terminfo?
+bindkey "\e[1;2D" backward-shell-word
+bindkey "\e[1;2C"  forward-shell-word
