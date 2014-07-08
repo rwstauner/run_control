5035f58f36c273bc499f1112999ee81cf09bc777
Randy Stauner <randy@magnificent-tears.com>
2014-07-08 07:14:28 -0700

    Adjust syntax highlight styles and remove cursor mod
    The cursor highlighter doesn't keep up with shell word movements
    and if I'm not adjusting the style (standout) there's no reason to load the mod.

--- zsh/zsh.d/highlighter.zsh
+++ zsh/zsh.d/highlighter.zsh
@@ -4,7 +4,6 @@ source $ZSH_HIGHLIGHT_FILE || return
 
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(
   brackets
-  cursor
   main
   pattern
   root
@@ -29,25 +28,25 @@ ZSH_HIGHLIGHT_HIGHLIGHTERS=(
 # ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=red,bold'
 # ZSH_HIGHLIGHT_STYLES[reserved-word]='fg=yellow'
   ZSH_HIGHLIGHT_STYLES[alias]='fg=154'
-  ZSH_HIGHLIGHT_STYLES[builtin]='fg=046'
+  ZSH_HIGHLIGHT_STYLES[builtin]='fg=048'
   ZSH_HIGHLIGHT_STYLES[function]='fg=112'
-  ZSH_HIGHLIGHT_STYLES[command]='fg=046'
-  ZSH_HIGHLIGHT_STYLES[precommand]='fg=046,underline'
+  ZSH_HIGHLIGHT_STYLES[command]='fg=048'
+  ZSH_HIGHLIGHT_STYLES[precommand]='fg=048,underline'
 # ZSH_HIGHLIGHT_STYLES[commandseparator]='none'
-  ZSH_HIGHLIGHT_STYLES[hashed-command]='fg=046'
-  ZSH_HIGHLIGHT_STYLES[path]='fg=083'
-# ZSH_HIGHLIGHT_STYLES[path_prefix]='underline'
-# ZSH_HIGHLIGHT_STYLES[path_approx]='fg=yellow,underline'
+  ZSH_HIGHLIGHT_STYLES[hashed-command]='fg=048'
+  ZSH_HIGHLIGHT_STYLES[path]='fg=086'
+  ZSH_HIGHLIGHT_STYLES[path_prefix]='fg=115'
+  ZSH_HIGHLIGHT_STYLES[path_approx]='fg=150'
 # ZSH_HIGHLIGHT_STYLES[globbing]='fg=blue'
 # ZSH_HIGHLIGHT_STYLES[history-expansion]='fg=blue'
-# ZSH_HIGHLIGHT_STYLES[single-hyphen-option]='none'
-# ZSH_HIGHLIGHT_STYLES[double-hyphen-option]='none'
+  ZSH_HIGHLIGHT_STYLES[single-hyphen-option]='fg=144'
+  ZSH_HIGHLIGHT_STYLES[double-hyphen-option]='fg=147'
 # ZSH_HIGHLIGHT_STYLES[back-quoted-argument]='none'
   ZSH_HIGHLIGHT_STYLES[single-quoted-argument]='fg=216'
   ZSH_HIGHLIGHT_STYLES[double-quoted-argument]='fg=213'
 # ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]='fg=cyan'
 # ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]='fg=cyan'
-# ZSH_HIGHLIGHT_STYLES[assign]='none'
+  ZSH_HIGHLIGHT_STYLES[assign]='fg=211'
 
 # pattern
 ZSH_HIGHLIGHT_PATTERNS+=('rm *' 'fg=white,bold,bg=red')
