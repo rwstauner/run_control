9d772f993061e6c210f6960fbd0866fafd3aeefa
Randy Stauner <randy@magnificent-tears.com>
2014-08-19 21:48:49 -0700

    Customize oh-my-zsh vars and disable unused features

--- zsh/ohmy.zsh
+++ zsh/ohmy.zsh
@@ -1,6 +1,5 @@
 # Path to your oh-my-zsh installation.
 export ZSH=$HOME/run_control/zsh/ext/oh-my-zsh
-ZSH_CUSTOM=$HOME/run_control/zsh/custom
 
 # Set name of the theme to load.
 # Look in ~/.oh-my-zsh/themes/
@@ -29,26 +28,26 @@ ZSH_CUSTOM=$HOME/run_control/zsh/custom
 # DISABLE_LS_COLORS="true"
 
 # Uncomment the following line to disable auto-setting terminal title.
-# DISABLE_AUTO_TITLE="true"
+DISABLE_AUTO_TITLE="true"
 
 # Uncomment the following line to disable command auto-correction.
 # DISABLE_CORRECTION="true"
 
 # Uncomment the following line to display red dots whilst waiting for completion.
-# COMPLETION_WAITING_DOTS="true"
+COMPLETION_WAITING_DOTS="true"
 
 # Uncomment the following line if you want to disable marking untracked files
 # under VCS as dirty. This makes repository status check for large repositories
 # much, much faster.
-# DISABLE_UNTRACKED_FILES_DIRTY="true"
+DISABLE_UNTRACKED_FILES_DIRTY="true"
 
 # Uncomment the following line if you want to change the command execution time
 # stamp shown in the history command output.
 # The optional three formats: "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
-# HIST_STAMPS="mm/dd/yyyy"
+HIST_STAMPS="yyyy-mm-dd"
 
 # Would you like to use another custom folder than $ZSH/custom?
-# ZSH_CUSTOM=/path/to/new-custom-folder
+ZSH_CUSTOM=$HOME/run_control/zsh/custom
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
@@ -89,9 +88,31 @@ plugins=(
   virtualenv
 )
 
+# Don't load OMZ libs we don't want to use.
+skip_omz_lib=(
+  aliases
+  bzr
+  directories
+  history
+  nvm
+  prompt_info_functions
+)
+
+source () {
+  [[ "$1" =~ "$ZSH/lib/(${(j:|:)skip_omz_lib}).zsh" ]] && return
+  builtin source "$@"
+}
+
 source $ZSH/oh-my-zsh.sh
 
+unset skip_omz_lib
+unset -f source
+
 # User configuration
+
+# Remove these hooks so they don't even run (better than DISABLE_AUTO_TITLE).
+add-zsh-hook -d precmd  omz_termsupport_precmd
+add-zsh-hook -d preexec omz_termsupport_preexec
 
 # export MANPATH="/usr/local/man:$MANPATH"
 
