f94cb3b3096e28c1b7e45c17c19e32c1d41be4b6
Randy Stauner <randy@magnificent-tears.com>
2014-09-09 06:54:37 -0700

    Disable auto-update check for zsh

--- zsh/ohmy.zsh
+++ zsh/ohmy.zsh
@@ -15,7 +15,7 @@ export ZSH=$HOME/run_control/zsh/ext/oh-my-zsh
 # CASE_SENSITIVE="true"
 
 # Uncomment the following line to disable bi-weekly auto-update checks.
-# DISABLE_AUTO_UPDATE="true"
+DISABLE_AUTO_UPDATE="true"
 
 # Uncomment the following line to change how often to auto-update (in days).
 # export UPDATE_ZSH_DAYS=13
