48ee65115a85e929f90a7561afa29ff3880437d0
Randy Stauner <randy@magnificent-tears.com>
2014-09-27 10:28:52 -0700

    Change 'history' command to show all and use pager

--- zsh/zsh.d/aliases.zsh
+++ zsh/zsh.d/aliases.zsh
@@ -1,5 +1,10 @@
 alias help=run-help
 
+
+# The built-in history is `fc -l` but that only shows 10.  Show all of them.
+# ...but use a pager because otherwise that will wipe out my scrollback.
+'history' () { fc -l 1 | $PAGER; }
+
 # -v verbose
 # -c differently verbose
 # -f show code for functions
