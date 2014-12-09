bbb1a40b157953c7f81130c559a38d9a3abae938
Randy Stauner <randy@magnificent-tears.com>
2014-12-08 20:12:11 -0700

    Use trailing colon on MANPATH so system paths are appended

--- sh/sh.d/man.sh
+++ sh/sh.d/man.sh
@@ -1,9 +1,16 @@
+# If MANPATH starts or ends with a colon the system config will be pre- or appended.
+export MANPATH="${MANPATH:-:}"
+
 add_to_manpath () {
   dir="$1"
   [[ -d "$dir" ]] || return
-  [[ -n "$MANPATH" ]] && MANPATH=":$MANPATH"
-  export MANPATH="$dir${MANPATH/:$dir/}"
+  # If MANPATH has contents (besides the colon we added above)
+  # prepend colon so we can do a simple s/:$dir// next.
+  [[ -n "${MANPATH%:}" ]] && MANPATH=":${MANPATH}"
+  MANPATH="$dir${MANPATH//:$dir/}"
 }
 
-add_to_manpath $HOME/usr/share/man
+# manpath will add this automatically because ~/usr/bin is in $PATH.
+#add_to_manpath $HOME/usr/share/man
+
 add_to_manpath $HOME/homebrew/opt/coreutils/libexec/gnuman
