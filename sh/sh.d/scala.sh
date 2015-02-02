ea0e6018da8b5ffa6e2c4d1e4e28969d1f3e5b08
Randy Stauner <randy@magnificent-tears.com>
2015-02-01 21:07:24 -0700

    Setup env vars for scala

---
+++ sh/sh.d/scala.sh
@@ -0,0 +1,14 @@
+# To root everything else.
+SCALA_ROOT=$HOME/scala
+
+# Enable tools (like vim) to find support files.
+export SCALA_DIST=$SCALA_ROOT/scala-dist
+
+# Actually scala installation.
+export SCALA_HOME=$SCALA_ROOT/local
+
+# Get scala.
+add_to_path $SCALA_HOME/bin
+
+# Get sbt.
+add_to_path $SCALA_ROOT/sbt/bin
