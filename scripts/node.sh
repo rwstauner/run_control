f5ffb7bb389e1f510e7e5d2cd041e309b96f4e02
Randy Stauner <randy@magnificent-tears.com>
2015-02-21 15:16:59 -0700

    Comment on installing io.js

--- scripts/node.sh
+++ scripts/node.sh
@@ -3,6 +3,9 @@
 . `dirname "$0"`/.helpers.sh
 umask 0077
 
+# https://iojs.org/dist/latest/
+# curl https://iojs.org/dist/latest/ | perl -lne 'print $1 if /href="(iojs-.+?linux-x64\.tar\.xz)"/'
+
 setup_runtime_dir node
 
 mkdir -p ~/node/global
