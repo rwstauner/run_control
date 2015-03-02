002347d6d2b49f1e789d5bb392d7ef614def25f8
Randy Stauner <randy@magnificent-tears.com>
2015-03-02 15:39:27 -0600

    Install imagemagick with homebrew
    and comment on how to update

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -22,6 +22,10 @@ brew install git
 brew install gnu-sed
 symlink gsed $brewbin/sed
 
+# If imagemagick has removed the version in the formula:
+# SHA256=foobar URL=yo perl -p -i -e 's/(^\s+(sha256|url)\s+").+?(")/$1$ENV{"\U$2"}$3/' Library/Formula/imagemagick.rb
+brew install imagemagick
+
 brew install jq
 
 #brew install imagesnap
