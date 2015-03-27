b68d2355e109168af3e39a823f8bcc2956d1270c
Randy Stauner <randy@magnificent-tears.com>
2015-03-26 20:51:37 -0700

    Script installation of io.js

--- scripts/node.sh
+++ scripts/node.sh
@@ -3,9 +3,24 @@
 . `dirname "$0"`/.helpers.sh
 umask 0077
 
-# https://iojs.org/dist/latest/
-# curl https://iojs.org/dist/latest/ | perl -lne 'print $1 if /href="(iojs-.+?linux-x64\.tar\.xz)"/'
+install_iojs () {
+  url="https://iojs.org/dist/latest/"
+  archive=`curl "$url" | perl -lne 'print $1 if /href="(iojs-.+?linux-x64\.tar\.xz)"/'`
+  basename="${archive%.tar.*}"
+  parent="$HOME/usr"
+  symlink="$parent/iojs"
+
+  if ! [[ -d "$parent/$basename" ]]; then
+    # Write to disk instead of pipe to utilize `tar -a`.
+    curl -O "$url/$archive" && \
+    tar -xaf "$archive" -C "$parent" && \
+    rm "$symlink" && ln -s "$basename" "$symlink" && \
+    rm "$archive"
+  fi
+}
 
 setup_runtime_dir node
 
 mkdir -p ~/node/global
+
+install_iojs
