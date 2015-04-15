4eb5ed72ea634c7d70265d4e0b1057a238a32fbd
Randy Stauner <randy@magnificent-tears.com>
2015-04-15 07:10:54 -0700

    Simplify java/jars env by expecting symlinks
    rather than searching every time.

--- sh/sh.d/java.sh
+++ sh/sh.d/java.sh
@@ -1,40 +1,21 @@
 # TODO: eval
-_find_home () {
-  var="$1"
-  name="$2"
 
-  # Stop if the var is already set.
-  if [[ -n "$ZSH_VERSION" ]]; then
-    [[ -z "${(P)var}" ]] || return
-  else
-    [[ -z "${!var}"   ]] || return
-  fi
+# Assume symlinks exist.
 
-  for dir in $HOME/java $HOME/opt /opt; do
-  for found in $dir/$name*${ZSH_VERSION:+(N)}; do
-    if [[ -d "$found" ]]; then
-      while test -h "$found"; do
-        found="`readlink "$found"`"
-      done
-      eval "export $var='$found'"
-      add_to_path $found/bin
-      break 2
-    fi
-  done
-  done
+java-home-vars () {
+  local name="$1" dir="$2"
+  [[ -d "$dir" ]] || return
+  add_to_path $dir/bin
+  export $name=$dir
 }
 
-  # prefer jdk, accept jre
-  _find_home JAVA_HOME jdk
-  _find_home JAVA_HOME jre
+# jar-alias () {
+#   local name="$1" pre="$2" post="$3"
+#   local jar=$HOME/java/jars/$name.jar
+#   [[ -f $jar ]] || return
+#   alias $name="$pre $jar${post:+ }$post"
+# }
 
-  _find_home GROOVY_HOME groovy
+java-home-vars JAVA_HOME ~/java/home
 
-  _find_home GRAILS_HOME grails
-
-unset -f _find_home
-
-mvel_jar=`ls $JAVA_HOME/../jars/mvel*.jar${ZSH_VERSION:+(N)} 2> /dev/null | tail -n 1`
-if [[ -n "$mvel_jar" ]]; then
-  alias mvel="rlwrap -C mvel java -jar $mvel_jar"
-fi
+#jar-alias mvel 'rlwrap -C mvel java -jar' ''
