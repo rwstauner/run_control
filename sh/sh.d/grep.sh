71e4349fea9bda124e9fb77195d70af51b08755c
Randy Stauner <randy@magnificent-tears.com>
2015-07-06 08:10:58 -0700

    Put grep line number in separate alias

--- sh/sh.d/grep.sh
+++ sh/sh.d/grep.sh
@@ -1,6 +1,5 @@
 # putting color in GREP_OPTIONS makes it work for (b)zgrep, too
-# Always show line number to make it easy to use as a vim quickfix buffer.
-GREP_OPTIONS="--color=auto --line-number"
+GREP_OPTIONS="--color=auto"
 
 GREP_COLORS="ms=01;31:mc=01;33:sl=:cx=34:fn=35:ln=32:bn=32:se=36"
 export GREP_OPTIONS GREP_COLORS
@@ -10,3 +9,9 @@ export GREP_OPTIONS GREP_COLORS
 
 # Pipe requires shift but fingers are too fast/slow.
 alias Grep='grep'
+
+# Alias to show line number to make it easy to use as a vim quickfix buffer.
+# Use a separate alias rather than `grep` or `GREP_OPTIONS` since so many things
+# use grep that might not be expecting altered output.
+# Also force display of filename for consistency.
+alias grepn='grep -H -n'
