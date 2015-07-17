864ea47f940bc467e3f922e2802b3ce5f46a7bd2
Randy Stauner <randy@magnificent-tears.com>
2015-07-17 16:27:03 -0700

    Enable node repl hist file

--- sh/sh.d/node.sh
+++ sh/sh.d/node.sh
@@ -7,3 +7,5 @@ add_to_path $node_global/bin
 # FIXME: Is this necessary?  Is it correct?
 NODE_PATH=$NODE_PATH:$node_global/lib/node_modules
 export NODE_PATH
+
+export NODE_REPL_HISTORY_FILE=$HOME/node/.hist
