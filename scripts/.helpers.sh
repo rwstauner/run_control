75a0fd59a4c3b55c64913d499bffc7845a172691
Randy Stauner <randy@magnificent-tears.com>
2015-07-25 10:16:26 -0700

    Rename and simplify installer for ag

--- scripts/.helpers.sh
+++ scripts/.helpers.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+set -x
+umask 0022
+
+PREFIX=$HOME/usr
+SRC_DIR=$HOME/data/src
+
+have () {
+  which "$1" &> /dev/null
+}
+
+git-dir () {
+  local url="$1" dir="$2"
+  if ! [[ -d "$dir" ]]; then
+    git clone "$url" "$dir"
+  fi
+  cd "$dir"
+  # Stay in dir.
+}
+
+git-checkout-latest-tag () {
+  if [[ -d .git ]]; then
+    git checkout master
+    git pull "$@"
+    # Checkout latest tag (strip describe's commits since tag).
+    # 0.30.0-24-g07f8336 => 0.30.0
+    git checkout `git describe --tags | sed -r 's/-[0-9]+-g[0-9a-f]+$//'`
+  fi
+}
+
 version_ge () {
   perl -e '($s, $t) = map { [split /\./, (/([0-9.]+)/)[0]] } @ARGV; while(@$t){ shift(@$s) >= shift(@$t) or exit(1) }' -- "$@"
 }
