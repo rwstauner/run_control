ea37f6d546f970f66455c3ebe450a9af8cfd8231
Randy Stauner <randy@magnificent-tears.com>
2015-07-25 19:28:31 -0700

    Abstract archive dl/extract for re-use

--- scripts/.helpers.sh
+++ scripts/.helpers.sh
@@ -20,7 +20,7 @@ arch-info () {
 }
 
 download () {
-  local url="$1" dest="$2"
+  local url="$1" dest="${2:-${1##*/}}"
   wget "$url" -O "$dest"
 }
 
@@ -50,6 +50,23 @@ git-checkout-latest-tag () {
     # Checkout latest tag (strip describe's commits since tag).
     # 0.30.0-24-g07f8336 => 0.30.0
     git checkout `git describe --tags | sed -r 's/-[0-9]+-g[0-9a-f]+$//'`
+  fi
+}
+
+versioned-archive-dir () {
+  local name="$1" url="$2";
+  local basename="${url##*/}"
+  local dest="$PREFIX/${basename%.tar.*}"
+  local symlink="$name"
+
+  if ! [[ -d "$dest" ]]; then
+    cd "${dest%/*}" && \
+    # Write to disk instead of pipe to utilize `tar -a`.
+    download "$url" && \
+    tar -xaf "$archive" && \
+    rm -f "$symlink" && \
+    ln -s "${dest##*/}" "$symlink" && \
+    rm "$archive"
   fi
 }
 
