0ac9722bd5852a804e412178119a747162ef7452
Randy Stauner <randy@magnificent-tears.com>
2015-08-02 21:26:22 -0700

    Simplify git_config script with functions

--- scripts/git_config.sh
+++ scripts/git_config.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# vim: set fdm=marker:
 
 # Turn off xtrace that helpers turns on (too much noise for this).
 . "${0%/*}/.helpers.sh" +x
@@ -16,25 +17,22 @@ fi
 
 git_version=`git --version`
 
+# helper functions {{{
 
-function have_git_version () { version_ge "$git_version" "$1"; }
-
-gc="git config --global"
-config () {
-  #echo "$@"
-  $gc "$@"
+have_git_version () {
+  version_ge "$git_version" "$1";
 }
 
-# [settings]
-# TODO: figure out $gc core.autocrlf        input
+config () {
+  git config --global "$@"
+}
 
-$gc color.ui             auto
-
-$gc core.excludesFile ~/.excludes
-
-config rebase.autosquash true
-
-config grep.lineNumber   true
+alias () {
+  local name="$1" cmd="$2"
+  # If it's an external command that uses any argument vars wrap it in a function.
+  [[ "${cmd:0:1}" == "!" ]] && [[ "$cmd" =~ \$\{?[1-9@*] ]] && cmd="!_() { ${2#!}; }; _"
+  config alias."$name" "$cmd"
+}
 
 grep_color () {
   local slot="$1" sgr="$2" val=""
@@ -74,163 +72,181 @@ parse_grep_colors () {
     esac
   done
 }
+
+# }}}
+
 parse_grep_colors
 
+# [settings]
+# TODO: figure out config core.autocrlf        input
+
+config color.ui          auto
+
+config core.excludesFile ~/.excludes
+
+config rebase.autosquash true
+
+config grep.lineNumber   true
+
 # make tabs whitespace errors
-#$gc core.whitespace      "trailing-space,space-before-tab,tab-in-indent"
+#config core.whitespace      "trailing-space,space-before-tab,tab-in-indent"
 
-$gc merge.log            true
+config merge.log            true
 
-$gc instaweb.local       true
-$gc instaweb.httpd       "/usr/sbin/apache2 -f"
-$gc instaweb.port        4321
-$gc instaweb.modulepath  /usr/lib/apache2/modules
+config instaweb.local       true
+config instaweb.httpd       "/usr/sbin/apache2 -f"
+config instaweb.port        4321
+config instaweb.modulepath  /usr/lib/apache2/modules
 
-$gc sendemail.confirm    always
-$gc sendemail.multiedit  true
+config sendemail.confirm    always
+config sendemail.multiedit  true
 
-$gc web.browser          firefox
+config web.browser          firefox
 
 if have_git_version 1.7.5; then
-  $gc push.default         upstream
+  config push.default         upstream
 else
   # deprecated synonym
-  $gc push.default         tracking
+  config push.default         tracking
 fi
 
 # https://github.com/git/git/tree/master/contrib/diff-highlight
 # NOTE: this might need --color
-# for cmd in log show diff; { $gc pager.$cmd "diff-highlight | $PAGER"; }
+# for cmd in log show diff; { config pager.$cmd "diff-highlight | $PAGER"; }
 
 # [diff helpers]
 # use with ".gitattributes": '*.png diff=exif'
 
-$gc diff.exif.textconv    'exiftool'; # exiv2 ?
-$gc diff.strings.textconv 'strings'
-$gc diff.ziplist.textconv 'unzip -l'
-$gc diff.pdftext.textconv 'pdftotext'
+config diff.exif.textconv    'exiftool'; # exiv2 ?
+config diff.strings.textconv 'strings'
+config diff.ziplist.textconv 'unzip -l'
+config diff.pdftext.textconv 'pdftotext'
 
 # [aliases]
 # NOTE: git uses 'sh'
 # find some nice examples at: https://git.wiki.kernel.org/index.php/Aliases
 
-$gc alias.exec            $'!_() { exec "$@"; }; _'
+# Execute a command from git root with git env.
+# Use function so that we get to sh's builtin.
+alias exec '!exec "$@"'
 
-$gc alias.add-p           $'add -p'
-$gc alias.adp             $'add -p'
-$gc alias.touch           $'add -N'
+alias add-p           'add -p'
+alias adp             'add -p'
+alias touch           'add -N'
 
-$gc alias.b               $'branch'
-$gc alias.bv              $'branch  -vv'
-$gc alias.bav             $'branch -avv'
-$gc alias.bnm             $'branch --no-merged'
+alias b               'branch'
+alias bv              'branch  -vv'
+alias bav             'branch -avv'
+alias bnm             'branch --no-merged'
 
 # consider -C -C -C30
-$gc alias.blamehard       $'blame -w -C -C -C'
+alias blamehard       'blame -w -C -C -C'
 
-#$gc alias.bunch           $'!env FILE_LOG_LEVEL=off gitbunch'
-#$gc alias.bunch           $'!gitbunch'
+#alias bunch           '!env FILE_LOG_LEVEL=off gitbunch'
+#alias bunch           '!gitbunch'
 
-$gc alias.civ             $'commit -v'
-$gc alias.amend           $'commit -v -n --amend'
+alias civ             'commit -v'
+alias amend           'commit -v -n --amend'
 
-$gc alias.co              $'checkout'
-$gc alias.cob             $'checkout -b'
-$gc alias.com             $'checkout master'
-$gc alias.cobt            $'!_() { git branch-track "$@"; git co "$1"; }; _'
+alias co              'checkout'
+alias cob             'checkout -b'
+alias com             'checkout master'
 
-$gc alias.cpan-mailmap    $'!echo "`git config user.name` <`awk \047/^user / { print tolower($2) }\047 ~/.pause`@cpan.org> <`git config user.email`>"'
+alias cobt            '!git branch-track "$@"; git co "$1"'
+
+# TODO: heredoc?
+alias cpan-mailmap    $'!echo "`git config user.name` <`awk \047/^user / { print tolower($2) }\047 ~/.pause`@cpan.org> <`git config user.email`>"'
 
 # clone repo, make remote "origin" for first arg and "upstream" for second
-$gc alias.clone-fork      $'!_() { fork=$1 upstream=$2; forkdir=${3:-${fork##*/}}; forkdir=${forkdir%.git}; git clone $fork; cd ${forkdir}; git remote add upstream $upstream; }; _'
+alias clone-fork      '!fork=$1 upstream=$2; forkdir=${3:-${fork##*/}}; forkdir=${forkdir%.git}; git clone $fork; cd ${forkdir}; git remote add upstream $upstream'
 
 # git rev-parse --default master --symbolic-full-name HEAD
-$gc alias.current-branch  $'!git symbolic-ref HEAD | sed s-^refs/heads/--'
+alias current-branch  '!git symbolic-ref HEAD | sed s-^refs/heads/--'
 
 # diff diff diff
-$gc alias.vimdiff         $'!vim +Gdiff'
+alias vimdiff    '!vim +Gdiff'
 
 git_ix='diff --cached -M --minimal'
-$gc alias.ix               "$git_ix"
+alias ix         "$git_ix"
 
-$gc alias.diffstat        $'diff --stat -r'
-$gc alias.diffst          $'diff --stat -r'
+alias diffstat   'diff --stat -r'
+alias diffst     'diff --stat -r'
 
 # show char-by-char (or word-by-word) differences instead of whole lines
-function alias_diffs () {
+alias_diffs () {
   alias="$1" prefix="$2" suffix="$3"
-  $gc "alias.ix$alias"     "$prefix${prefix:+ }${git_ix} ${suffix}"
-  $gc "alias.diff$alias"   "$prefix${prefix:+ }diff $suffix"
-  $gc "alias.logp$alias"   "$prefix${prefix:+ }log -p $suffix"
-  $gc "alias.showp$alias"  "$prefix${prefix:+ }show -p $suffix"
+  config "alias.ix$alias"     "$prefix${prefix:+ }${git_ix} ${suffix}"
+  config "alias.diff$alias"   "$prefix${prefix:+ }diff $suffix"
+  config "alias.logp$alias"   "$prefix${prefix:+ }log -p $suffix"
+  config "alias.showp$alias"  "$prefix${prefix:+ }show -p $suffix"
 }
 
 alias_diffs cw            '' $'--color-words=.'
 alias_diffs cww           '' $'--color-words=\\\\w+'
 alias_diffs hl            '!_() { git ' ' --color "$@" | diff-highlight | $PAGER; }; _'
 
-$gc alias.diffwithsubs    $'!git submodule summary; git diff'
+alias diffwithsubs     '!git submodule summary; git diff'
 
 # the sha1 for an empty tree in case you want to compare something to a bare repo
-$gc alias.empty-tree-sha1 $'hash-object -t tree /dev/null'
+alias empty-tree-sha1  'hash-object -t tree /dev/null'
 
-#$gc alias.change-github-username $'!sed -i -re \047s/(github.com:)magnificent-tears/\\1rwstauner/\047 .git/config'
+#alias change-github-username $'!sed -i -re \047s/(github.com:)magnificent-tears/\\1rwstauner/\047 .git/config'
 
 # Create a script that can be verified and then piped to sh.
 # TODO: split args b/t log and fp with `--`.
-$gc alias.format-patch-script  $'!_() { file="$1"; shift; git log --oneline "$file" | perl -e \'print reverse <STDIN>\' | perl -lne \'BEGIN { @opts = splice @ARGV } ($c, $s) = split / /, $_, 2; print q[git format-patch -n --start-number ], ++$patch, qq[ @opts -1 $c # $s]\' -- "$@"; }; _'
+alias format-patch-script  $'!file="$1"; shift; git log --oneline "$file" | perl -e \'print reverse <STDIN>\' | perl -lne \'BEGIN { @opts = splice @ARGV } ($c, $s) = split / /, $_, 2; print q[git format-patch -n --start-number ], ++$patch, qq[ @opts -1 $c # $s]\' -- "$@"'
 
 # filenames at the top: --heading --break ?
-$gc alias.grep-todo       $'grep -iE "to.?do|fix.?me"'
+alias grep-todo       'grep -iE "to.?do|fix.?me"'
 
-$gc alias.homepage-metacpan      $'!curl -v -d "login=`git config github.user`&token=`git config github.token`&values[homepage]=http://metacpan.org/release/${PWD##*/}" "https://github.com/api/v2/json/repos/show/rwstauner/${PWD##*/}"'
+alias homepage-metacpan    '!curl -v -d "login=`git config github.user`&token=`git config github.token`&values[homepage]=http://metacpan.org/release/${PWD##*/}" "https://github.com/api/v2/json/repos/show/rwstauner/${PWD##*/}"'
 
 # uses patchutils/interdiff to see upstream modifications b/t two commits (origin/master and local branch)
-$gc alias.intercommit     $'!_() { git show "$1" > .git/commit1 && git show "$2" > .git/commit2 && interdiff .git/commit[12] | less -FRS; }; _'
+alias intercommit     '!git show "$1" > .git/commit1 && git show "$2" > .git/commit2 && interdiff .git/commit[12] | less -FRS'
 
-$gc alias.initial-commit  $'!git rev-list --all | tail -n 1'
+alias initial-commit  '!git rev-list --all | tail -n 1'
 
-# gitk
-$gc alias.k               $'!gitk'
+alias k                '!gitk'
 # Gitv (from the command line seems to require an argument)
-$gc alias.kv              $'!vim -c "silent Gitv" `if test "$#" -gt 0; then echo "$@"; else echo .; fi`'
+alias kv               '!vim -c "silent Gitv" `if test "$#" -gt 0; then echo "$@"; else echo .; fi`'
 
 # there must be a better way
-$gc alias.last-sha        $'!git show HEAD --oneline | head -n1 | awk \047{print $1}\047'
-$gc alias.last-tag        $'!git describe --tags --long | sed -re \047s/-[0-9]+-g[a-f0-9]+$//\047'
+alias last-sha        $'!git show HEAD --oneline | head -n1 | awk \047{print $1}\047'
+alias last-tag        $'!git describe --tags --long | sed -re \047s/-[0-9]+-g[a-f0-9]+$//\047'
 
-$gc alias.log1            $'log --oneline'
-$gc alias.logp            $'log -p'
+alias log1       'log --oneline'
+alias logp       'log -p'
 
-$gc alias.logdiff         $'log -p --full-diff'
-
-# show new commits after last fetch
-$gc alias.lc              $'log ORIG_HEAD.. --stat --no-merges'
-$gc alias.log-fetched     $'log ..FETCH_HEAD --stat --no-merges'
-# this should probably be log-remote and figure out the tracking remote
-$gc alias.log-origin      $'log ..origin --stat --no-merges'
+alias logdiff    'log -p --full-diff'
 
 # The --stat-name-width=${COLUMNS:-80} arg doesn't seem to help very much.
-$gc alias.logstat         $'log --stat'
-$gc alias.logst           $'log --stat --no-merges'
+logst='log --stat --no-merges'
+alias logstat         'log --stat'
+alias logst           "$logst"
 
-$gc alias.log-since-tag   $'!_() { tag=`git last-tag`; revs=$tag..HEAD; git log $* $revs; echo "\n=== $revs ===\n"; git tag-summary $tag; }; _'
+# show new commits after last fetch
+alias lc              "$logst ORIG_HEAD.."
+alias log-fetched     "$logst ..FETCH_HEAD"
+# this should probably be log-remote and figure out the tracking remote
+alias log-origin      "$logst ..origin"
+
+alias log-since-tag   $'!tag=`git last-tag`; revs=$tag..HEAD; git log "$@" $revs; echo "\n=== $revs ===\n"; git tag-summary $tag'
 
 # rjbs!
-$gc alias.hist            $'log --graph --all --color=always --decorate'
+alias hist             'log --graph --all --color=always --decorate'
 
 # condense log output (modified from http://coderwall.com/p/euwpig)
-$gc alias.lg               "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)%aN%Creset %C(blue)<%aE>%Creset' --abbrev-commit"
-$gc alias.lgpcw           $'!git lg -p --color-words=.'
+alias lg               "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)%aN%Creset %C(blue)<%aE>%Creset' --abbrev-commit"
+alias lgpcw            '!git lg -p --color-words=.'
 
-# ls-files doesn't tab-complete, so just shorten it
-$gc alias.ls              $'!_() { git ls-files "$@" | $PAGER; }; _'
-$gc alias.lsgrep          $'!_() { git ls-files | grep --color=always "$@" | $PAGER; }; _'
+# ls-files doesn't tab-complete, so shorten it, and force it through a pager.
+alias ls               '!git ls-files "$@" | $PAGER'
+alias lsgrep           '!git ls-files | grep --color=always "$@" | $PAGER'
 
-$gc alias.merge-delete    $'!_() { git merge "$1" && git branch -d "$1"; }; _'
+alias merge-delete     '!git merge "$1" && git branch -d "$1"'
 
 # provide rebasing music (https://coderwall.com/p/at9bya)
-#$gc alias.mav             $'!afplay ~/Music/Danger\\ Zone.mp3 & LASTPID=$! \ngit rebase -i $1 \nkill -9 $LASTPID\n true'
+#alias mav             $'!afplay ~/Music/Danger\\ Zone.mp3 & LASTPID=$! \ngit rebase -i $1 \nkill -9 $LASTPID\n true'
 
 # https://blog.afoolishmanifesto.com/posts/git-aliases-for-your-life/
 # git rebase --root --onto origin/master -i --autosquash
@@ -238,56 +254,57 @@ $gc alias.merge-delete    $'!_() { git merge "$1" && git branch -d "$1"; }; _'
 # gitk ^origin/master HEAD
 
 # what new commits have been created by the last command (like "git pull")
-$gc alias.new             $'!_() { git log $1@{1}..$1@{0} "$@"; }; _'
+alias new              '!git log $1@{1}..$1@{0} "$@"'
 
-$gc alias.prune-all       $'!git remote | xargs -n 1 git remote prune'
+alias prune-all        '!git remote | xargs -n 1 git remote prune'
 
-$gc alias.pulls           $'!git pull; test -f ${GIT_DIR:-.}/.gitmodules && git submodule update'
+alias pulls            '!git pull; test -f ${GIT_DIR:-.}/.gitmodules && git submodule update'
 
 # pum doesn't seem to be storing the fetch, so do both
-$gc alias.pum             $'!git fetch upstream; git pull upstream master'
+alias pum              '!git fetch upstream; git pull upstream master'
 
-$gc alias.pusht           $'!_() { git push "$@"; git push --tags "$@"; }; _'
+alias pusht            '!git push "$@"; git push --tags "$@"'
 
-$gc alias.branch-to-remote       $'!_() { branch=${1:-`git current-branch`} remote=${2:-origin}; git config branch.$branch.remote $remote; git config branch.$branch.merge refs/heads/$branch; }; _'
-$gc alias.branch-track    $'!_() { branch=${1} remote=${2:-origin}; git branch --track $branch remotes/$remote/$branch; }; _'
+alias branch-to-remote  '!branch=${1:-`git current-branch`} remote=${2:-origin}; git config branch.$branch.remote $remote; git config branch.$branch.merge refs/heads/$branch'
+alias branch-track      '!branch=${1} remote=${2:-origin}; git branch --track $branch remotes/$remote/$branch'
 
 # load pull requests as remote branches (github++)
 # https://gist.github.com/piscisaureus/3342247
-$gc alias.fetch-pr        $'!_() { remote="${1:-origin}"; git fetch "$remote" "+refs/pull/*/head:refs/remotes/$remote/pr/*"; }; _'
-$gc alias.branch-pr       $'!_() { branch="$1"; pr="$2"; remote="${3:-origin}"; git co -b "$branch" -t "refs/remotes/$remote/pr/$pr"; }; _'
+alias fetch-pr         '!remote="${1:-origin}"; git fetch "$remote" "+refs/pull/*/head:refs/remotes/$remote/pr/*"'
+alias branch-pr        '!branch="$1"; pr="$2"; remote="${3:-origin}"; git co -b "$branch" -t "refs/remotes/$remote/pr/$pr"'
 
 # Since git operates from the project root dir `pwd` also works.
 #$gc alias.root            $'!pwd'
-$gc alias.root            $'rev-parse --show-toplevel'
+alias root             'rev-parse --show-toplevel'
 
 # run daemon (use !git to run from repo root) then try git ls-remote
-$gc alias.serve           $'!git daemon --reuseaddr --verbose  --base-path=. --export-all ./.git'
+alias serve            '!git daemon --reuseaddr --verbose  --base-path=. --export-all ./.git'
 
-$gc alias.st              $'!_() { if [ $# -gt 0 ]; then git status "$@"; else git status && git stash list | sed -re "s/^([^:]+):/\\\033[33m\\1\\\033[00m:/"; fi; }; _'
+alias st              $'!if [ $# -gt 0 ]; then git status "$@"; else git status && git stash list | sed -re "s/^([^:]+):/\\\033[33m\\1\\\033[00m:/"; fi'
 
 # Use $PAGER and -p to force (only for this command) sending color to pipe.
-$gc alias.stpp            $'!PAGER=fppt git -p st'
+alias stpp             '!PAGER=fppt git -p st'
 
-$gc alias.s               $'status -s -b -u'
-$gc alias.tag-summary     $'!_() { git show --summary ${1:-`git last-tag`}; }; _'
+alias s                'status -s -b -u'
+alias tag-summary      '!git show --summary ${1:-`git last-tag`}'
 
-  $gc alias.would-update  "!_() { branch=\$(git symbolic-ref HEAD | sed s-^refs/heads/--); range=\"\${1:-..\`git config branch.\$branch.remote\`}/\$branch\"; { git log --color --graph --oneline \"\$range\"; git diff --color --stat \"\$range\"; git submodule-would-update; } | \${GIT_PAGER:-\${PAGER:-less -FRX}}; }; _"
+  alias would-update  "!branch=\$(git symbolic-ref HEAD | sed s-^refs/heads/--); range=\"\${1:-..\`git config branch.\$branch.remote\`}/\$branch\"; { git log --color --graph --oneline \"\$range\"; git diff --color --stat \"\$range\"; git submodule-would-update; } | \${GIT_PAGER:-\${PAGER:-less -FRX}}"
 
 # Version 1.6 doesn't have git diff --submodule.
 # Using git log lets us pass more options.
-  $gc alias.submodule-would-update-simple "!_() { range=\"\${1:-..\`git remote\`}\"; git submodule -q foreach 'echo \$path' | while read path; do git diff --submodule=log \"\$range\" \$path; done; }; _"
-  $gc alias.submodule-would-update-fancy  "!_() { range=\"\${1:-..\`git remote\`}\"; git submodule -q foreach 'echo \$path' | while read path; do submod_range=\`git diff \$range \$path | perl -le ' while(<>){ if( /^[-+]Subproject commit (\\S+)/ ){ push @c, \$1 } } print join q[..], @c'\`; (test -n \"\$submod_range\" && cd \$path && echo Submodule \$path \$submod_range && git log --color --exit-code --graph --oneline \"\$@\" \$submod_range); done | LESS= /usr/bin/less -FRX; }; _"
+  # TODO: heredocs?
+  alias submodule-would-update-simple "!range=\"\${1:-..\`git remote\`}\"; git submodule -q foreach 'echo \$path' | while read path; do git diff --submodule=log \"\$range\" \$path; done"
+  alias submodule-would-update-fancy  "!range=\"\${1:-..\`git remote\`}\"; git submodule -q foreach 'echo \$path' | while read path; do submod_range=\`git diff \$range \$path | perl -le ' while(<>){ if( /^[-+]Subproject commit (\\S+)/ ){ push @c, \$1 } } print join q[..], @c'\`; (test -n \"\$submod_range\" && cd \$path && echo Submodule \$path \$submod_range && git log --color --exit-code --graph --oneline \"\$@\" \$submod_range); done | LESS= /usr/bin/less -FRX"
 
 # We could do if have_git_version 1.7.0 to use 'git diff --submodule' but I
 # prefer the other options that I can pass to log.
-  $gc alias.submodule-would-update "!git submodule-would-update-fancy"
+  alias submodule-would-update "!git submodule-would-update-fancy"
 
 
 # whois takes a name or email
-$gc alias.whois           $'!_() { git log -i -1 --pretty="format:%an <%ae>\n" --author="$1"; }; _'
+alias whois           $'!git log -i -1 --pretty="format:%an <%ae>\n" --author="$1"'
 # whatis takes a commit name
-$gc alias.whatis          $'show -s --pretty="tformat:%h (%s, %ad)" --date=short'
+alias whatis           'show -s --pretty="tformat:%h (%s, %ad)" --date=short'
 
 # https://coderwall.com/p/wazznq
 # Undelete files from a COMMIT_HASH:
