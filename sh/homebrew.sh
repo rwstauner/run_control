90017482b42641cd38997c4f00039148ee57c0a1
Randy Stauner <randy@magnificent-tears.com>
2015-10-19 19:59:03 -0700

    Add homebrew's sbin to path

--- sh/homebrew.sh
+++ sh/homebrew.sh
@@ -2,7 +2,7 @@
 # or after installing homebrew on demand.
 
 # Get brew exec.
-add_to_path $HOME/homebrew/bin
+add_to_path $HOME/homebrew/{bin,sbin}
 
 # Get (unprefixed) GNU utils to override BSD utils.
 add_to_path $HOME/homebrew/opt/coreutils/libexec/gnubin
