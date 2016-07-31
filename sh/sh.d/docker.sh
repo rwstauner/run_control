4462b388191c502a02991ec9b0ae8e63dc259110
Randy Stauner <randy@magnificent-tears.com>
2016-07-31 11:32:35 -0700

    Add several docker helper functions
    and remove the docker-machine invocation

--- sh/sh.d/docker.sh
+++ sh/sh.d/docker.sh
@@ -1,7 +1,26 @@
-# If docker-machine is installed load the env but only when docker is first used.
-docker () {
-  [ -d ~/.docker/machine/machines/default ] && \
-    eval $(docker-machine env default)
-  unset -f docker
-  command docker "$@"
+drun () {
+  args=(
+    -i --rm -v $PWD:/src -w /src
+  )
+  # Only specify -t if stdin is console (piping data to -t makes it mad).
+  test -t 0 && args+=-t
+  command docker run "${args[@]}" "$@"
+}
+
+docker-clean-images () {
+  docker images -q -f 'dangling=true' | xargs -r docker rmi
+}
+docker-clean-containers () {
+  docker ps     -q -f 'status=exited' | xargs -r docker rm
+}
+
+docker-in-docker-args () {
+  if [[ -n "$DOCKER_MACHINE_IP" ]]; then
+    # This won't work if the value contains a space,
+    # but leaving the double quotes means they become part of the value.
+    docker-machine env | perl -ne 'print " -e $1=$2 " if /export (.+?)="(.+?)"$/'
+    echo -n ${DOCKER_CERT_PATH:+"-v $DOCKER_CERT_PATH:$DOCKER_CERT_PATH"}
+  else
+    echo ' -v /var/run/docker.sock:/var/run/docker.sock '
+  fi
 }
