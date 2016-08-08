04d54260f264d22e2bc25b9be82f3669aeb21c59
Randy Stauner <randy@magnificent-tears.com>
2016-08-08 07:29:42 -0700

    Only unset lower-case tmux vars

--- tmux/unset-vars.sh
+++ tmux/unset-vars.sh
@@ -2,5 +2,6 @@
 
 # It's handy to set vars in tmux conf files for interpolation
 # but we don't need them to persist in the global env.
-perl -lne 'print "tmux setenv -g -u $1" if /^(\w+)=/' \
+# Assume that lower-case vars can be unset (allow upper case vars to persist).
+perl -lne 'print "tmux setenv -g -u $1" if /^([a-z0-9_]+)=/' \
   ~/.tmux.conf ~/run_control/tmux/*.conf | sh
