ff88597ebb513ab9e9b02fcf5d9d677009ab2c63
Randy Stauner <randy@magnificent-tears.com>
2017-03-23 10:44:35 -0700

    Install a shim for node/npm and use n to install other versions
    so much faster/simpler

--- sh/sh.d/node.sh
+++ sh/sh.d/node.sh
@@ -1,16 +1,6 @@
 add_to_path ~/usr/node/bin
 
-node_global=$HOME/node/global
-add_to_path $node_global/bin
-#add_to_path ./node_modules/.bin
-
-# FIXME: Is this necessary?  Is it correct?
-NODE_PATH=$NODE_PATH:$node_global/lib/node_modules
-export NODE_PATH
-
 export NODE_REPL_HISTORY_FILE=$HOME/node/.hist
+export N_PREFIX=$HOME/node/n
 
-export NVM_DIR=$HOME/node/nvm
-export NVM_LAZY_LOAD=true
-export NVM_NO_USE=true
-source_rc_files $HOME/node/.nvm.sh
+
