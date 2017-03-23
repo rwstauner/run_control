93c1b7cc4ba29246181daa1e7660996ddfcd7eea
Randy Stauner <randy@magnificent-tears.com>
2017-03-23 10:19:30 -0700

    Switch to zsh-nvm plugin for installation/lazy load

--- sh/sh.d/node.sh
+++ sh/sh.d/node.sh
@@ -9,3 +9,8 @@ NODE_PATH=$NODE_PATH:$node_global/lib/node_modules
 export NODE_PATH
 
 export NODE_REPL_HISTORY_FILE=$HOME/node/.hist
+
+export NVM_DIR=$HOME/node/nvm
+export NVM_LAZY_LOAD=true
+export NVM_NO_USE=true
+source_rc_files $HOME/node/.nvm.sh
