4a103993d33be2b83d5d5fefa9ccf4f88c279609
Randy Stauner <randy@magnificent-tears.com>
2017-04-21 20:37:36 -0700

    Install npm-bin to find the right executable

--- sh/sh.d/node.sh
+++ sh/sh.d/node.sh
@@ -3,4 +3,22 @@ add_to_path ~/usr/node/bin
 export NODE_REPL_HISTORY_FILE=$HOME/node/.hist
 export N_PREFIX=$HOME/node/n
 
+eslint () {
+  local changed
+  case "$*" in
+    last-changed|changed)
+      git "$1" | awk '/\.js$/ { printf "\0%s", $0 }' | IFS=$'\0' read -A changed
+      set -- "${changed[@]}"
+      shift; # The first element is empty.
+      ;;
+  esac
+  npm-bin eslint "$@"
+}
 
+for i in \
+  grunt \
+  karma \
+  stylelint \
+; {
+  alias $i="npm-bin $i"
+}
