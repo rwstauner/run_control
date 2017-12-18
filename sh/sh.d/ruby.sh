0fe48238b94911029929a966d8fd43713a3ef02e
Randy Stauner <randy@magnificent-tears.com>
2017-12-17 17:13:20 -0700

    Update ruby env

--- sh/sh.d/ruby.sh
+++ sh/sh.d/ruby.sh
@@ -7,3 +7,13 @@ if [[ -d $RBENV_ROOT ]]; then
 
   eval "$(rbenv init -)"
 fi
+
+export DISABLE_SPRING=1
+
+rbenv-gem-dir () {
+  echo $(rbenv prefix)/lib/ruby/gems/*/gems/
+}
+
+# If a Gemfile has rb-readline, rails console is fine.
+# Loading pry without rails requires including it directly.
+alias pry='pry -rrb-readline'
