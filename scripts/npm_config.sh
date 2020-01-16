543d2729e1d073962249dd34f69164f303d87e45
Randy Stauner <randy@magnificent-tears.com>
2020-01-16 08:59:23 -0700

    Remove all traces of node/npm

--- scripts/npm_config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-config () {
-  npm config set "$1" "$2" --global
-}
-
-[[ -w `which node` ]] || \
-config prefix $HOME/node/global
--- sh/sh.d/node.sh
+++ sh/sh.d/node.sh
@@ -1,25 +1,7 @@
-add_to_path ~/usr/node/bin
-
-export NODE_REPL_HISTORY_FILE=$HOME/node/.hist
-export N_PREFIX=$HOME/node/n
-
-eslint () {
-  local changed
-  case "$*" in
-    last-changed|changed)
-      git "$1" | awk '/\.js$/ { printf "\0%s", $0 }' | IFS=$'\0' read -A changed
-      set -- "${changed[@]}"
-      shift; # The first element is empty.
-      ;;
-  esac
-  npm-bin eslint "$@"
-}
-
-for i in \
-  grunt \
-  karma \
-  stylelint \
-  yarn \
-; {
-  alias $i="npm-bin $i"
+dnode () {
+  drunw \
+    -v "$HOME/.npmrc:/root/.npmrc:cached,ro" \
+    -v node_modules:/src/node_modules \
+    ${PORT+-p} ${PORT+$PORT:$PORT} \
+    "${IMAGE:-node:lts}" "$@"
 }
