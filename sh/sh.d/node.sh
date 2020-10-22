49d51f1926f3576b0c9a34057fe007f5d962471d
Randy Stauner <randy@magnificent-tears.com>
2020-10-22 15:03:49 -0700

    Allow passing docker args via env to node alias

--- sh/sh.d/node.sh
+++ sh/sh.d/node.sh
@@ -1,5 +1,7 @@
 dnode () {
+  [[ "$DNODE_ARGS" = "" ]] && DNODE_ARGS=()
   drunw \
+    "${DNODE_ARGS[@]}" \
     -v "$HOME/.npmrc:/root/.npmrc:cached,ro" \
     -v node_modules:/src/node_modules \
     ${PORT+-p} ${PORT+$PORT:$PORT} \
