6434f077acdd010310e9b53a89dba06aff87c84a
Randy Stauner <randy@magnificent-tears.com>
2020-12-14 21:28:18 -0700

    Ignore some shellcheck linters

--- sh/sh.d/env.sh
+++ sh/sh.d/env.sh
@@ -5,6 +5,12 @@ export FTP_PASSIVE=1; # used by Net::FTP, and maybe possibly hopefully some othe
 
 #export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig/:/usr/lib/pkgconfig/
 
+# SC1090: Can't follow non-constant source. Use a directive to specify lo⦉cation.
+# SC2006: Use $(...) notation instead of legacy backticked `...`.
+# SC2164: Use 'cd ... || exit' or 'cd ... || return' in case cd fails.
+# SC2155: Declare and assign separately to avoid masking return values.
+export SHELLCHECK_OPTS="-e SC2006 -e SC2164 -e SC2155"
+
 # Universal file exclusions
 export TAR_OPTIONS="$TAR_OPTIONS --exclude-from=$HOME/.excludes"
 
