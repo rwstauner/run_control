e045f2e324c8f962f446659495e67bd36d39c2fb
Randy Stauner <randy@r4s6.net>
2021-12-07 15:49:57 -0700

    Change global clj alias for nrepl to avoid clashes

--- sh/sh.d/clojure.sh
+++ sh/sh.d/clojure.sh
@@ -1,6 +1,6 @@
 clj () {
   if [[ $# -eq 0 ]]; then
-    clojure -M:cljfmt:nrepl:rebel:tools/repl -i ~/.clojure/src/my_repl.clj -m nrepl.cmdline -i -f my-repl/repl
+    clojure -M:cljfmt:nrepl-dep:rebel:tools/repl -i ~/.clojure/src/my_repl.clj -m nrepl.cmdline -i -f my-repl/repl
   else
     command clj "$@"
   fi
