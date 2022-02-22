14b8d0a9514630d13e6fa10ad19d9d93a28d4574
Randy Stauner <randy@r4s6.net>
2022-02-22 11:50:03 -0700

    Make fzf zsh script work with os install

--- zsh/zsh.d/fzf.zsh
+++ zsh/zsh.d/fzf.zsh
@@ -1,9 +1,7 @@
 # ~/.fzf.zsh {{{
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == *$HOME/usr/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}$HOME/usr/fzf/bin"
-fi
+add_to_path ~/usr/fzf/bin
 
 # Auto-completion
 # ---------------
@@ -11,7 +9,17 @@ fi
 
 # Key bindings
 # ------------
-source "$HOME/usr/fzf/shell/key-bindings.zsh"
+() {
+  local i
+  for i in \
+    "$HOME/usr/fzf/shell/key-bindings.zsh" \
+    "/usr/share/doc/fzf/examples/key-bindings.zsh" \
+  ; do
+    if [[ -r "$i" ]]; then
+      source "$i" && break
+    fi
+  done
+}
 # }}}
 
 
