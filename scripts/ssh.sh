3c89529a8379c591793b175c6d850a0f2ef0d3eb
Randy Stauner <randy@r4s6.net>
2022-02-23 11:13:56 -0700

    Setup defaults for ssh connection management

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -1,3 +1,19 @@
-#!/bin/bas
+#!/bin/bash
 
 mkdir -p ~/.ssh/.c
+
+perl -0777 -p -i -e '
+
+  $_ =~ s/\n*^# managed by run_control.+ \{\{\{.+^# \}\}\}\n*//smg;
+  $_ .= "
+
+# managed by run_control/'"$0"' {{{
+Host *
+  Compression yes
+  ControlMaster auto
+  # keep this as short as possible
+  ControlPath ~/.ssh/.c/%h%p%r
+  ControlPersist 5m
+# }}}
+";
+  ' ~/.ssh/config
