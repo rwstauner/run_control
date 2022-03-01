e02f2b1a9081f3a939a973e6944b5620e2aa0637
Randy Stauner <randy@r4s6.net>
2022-03-01 15:54:02 -0700

    Ensure WORDCHARS is empty

--- zsh/pre-load.zsh
+++ zsh/pre-load.zsh
@@ -1,3 +1,6 @@
 # Link emacs to main before doing anything else
 # (so that a later call to `bindkey -e` does not wipe out bindings).
 bindkey -e
+
+# A list of non-alphanumeric characters considered part of a word by the line editor.
+WORDCHARS=''
