34d96ef47e1712cb41d2e54f17e56bdb50bd133a
Randy Stauner <randy@r4s6.net>
2022-05-11 15:58:45 -0700

    Just use vim if nvim isn't present

--- sh/sh.d/vim.sh
+++ sh/sh.d/vim.sh
@@ -1,13 +1,17 @@
-alias vim=nvim
+if command -v nvim >&-; then
+  alias vim=nvim
+  export EDITOR="${EDITOR:-nvim}"
 
-export EDITOR="${EDITOR:-nvim}"
+  alias nvt='nvim +terminal'
+
+  if [[ -n "$NVIM_LISTEN_ADDRESS" ]]; then
+    alias nvim='nvr'
+  fi
+else
+  export EDITOR="${EDITOR:-vim}"
+fi
+
 export FCEDIT="$EDITOR" VISUAL="$EDITOR"
 
 alias vimXcat='ex -c w\ !\ cat -c :q'
 alias :sp='vim'
-
-alias nvt='nvim +terminal'
-
-if [[ -n "$NVIM_LISTEN_ADDRESS" ]]; then
-  alias nvim='nvr'
-fi
