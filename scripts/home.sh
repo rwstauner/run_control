96129633dab2789ca80cc7af6799ed415a0e88ef
Randy Stauner <randy@r4s6.net>
2022-07-21 17:53:31 -0700

    Move logic from Makefile to bash script

---
+++ scripts/home.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+dir=run_control
+
+find-files () {
+  find home/ -mindepth 1 -maxdepth 1 2>&- | sort
+}
+
+link-to-home () {
+  local src="$dir/$1"
+  local dest="$HOME/${1##*/}"
+  local filedesc=''
+
+  if [[ -e "$dest" ]]; then
+    if [[ "`readlink "$dest"`" == "$src" ]]; then
+      echo -e " \033[33m already linked: $dest \033[00m " 1>&2
+    else
+      [[ -e "$dest" ]] && ! [[ -s "$dest" ]] && filedesc="empty"
+      echo "  skipping pre-existing $filedesc file: $dest"
+    fi
+  else
+    if [[ -h "$dest" ]]; then
+      echo "removing broken symlink $dest (`readlink $dest`)"
+      rm -vf "$dest"
+    fi
+    echo ln -s "$src" "$dest"
+    ln -s "$src" "$dest"
+  fi
+}
+
+find-files | while read file; do
+  link-to-home "$file"
+done
+
+touch ~/.tmux.conf.local
