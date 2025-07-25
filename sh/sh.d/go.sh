c568b7d621a20c1bfe47dec2ff14eda09bfdd3c1
Randy Stauner <randy@r4s6.net>
2025-07-25 09:05:57 -0700

    Modernize go env: Set GOBIN and unset GOROOT

--- sh/sh.d/go.sh
+++ sh/sh.d/go.sh
@@ -1,8 +1,4 @@
-if [[ -d ~/go ]]; then
-  export GOROOT="$HOME/go"
-  add_to_path "$GOROOT/bin"
-fi
-
 export GOPATH="$HOME/gopath"
+export GOBIN="$GOPATH/bin"
 export GO111MODULE="on"
-add_to_path "$GOPATH/bin"
+add_to_path "$GOBIN"
