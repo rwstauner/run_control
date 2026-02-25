f8f69957d79c98897308928a500e92be0e5a7f80
Randy Stauner <randy@r4s6.net>
2026-02-25 11:18:48 -0700

    Put ruby env vars to unset in one place

--- sh/sh.d/ruby.sh
+++ sh/sh.d/ruby.sh
@@ -17,15 +17,21 @@ fi
 export DISABLE_SPRING=1
 export RUBY_CONFIGURE_OPTS=--disable-shared
 
+_unset_ruby_env_vars () {
+  unset RUBY_ROOT RUBY_ENGINE RUBY_VERSION GEM_ROOT GEM_HOME GEM_PATH BUNDLE_APP_CONFIG
+}
+
+# Overwrite chruby functions so dev doesn't force chruby into my shell.
 chruby () {
   if [[ $# -eq 1 ]]; then
+    echo export RBENV_VERSION="$1" 2>&1
     export RBENV_VERSION="$1"
   else
     rbenv versions
   fi
 }
 chruby_reset () {
-  unset RUBY_ROOT RUBY_ENGINE RUBY_VERSION GEM_ROOT GEM_HOME GEM_PATH
+  _unset_ruby_env_vars
   unset RBENV_VERSION
 }
 
@@ -73,7 +79,7 @@ ruby () {
 
   (
     if [[ -n "$RBENV_VERSION" ]]; then
-      unset RUBY_ROOT RUBY_ENGINE RUBY_VERSION GEM_ROOT GEM_HOME GEM_PATH BUNDLE_APP_CONFIG
+      _unset_ruby_env_vars
     fi
     # Prepend bindir to PATH to so that "bundler exec ruby" will invoke the right executable.
     PATH=${ruby%/*}:$PATH command "$ruby" "$@"
